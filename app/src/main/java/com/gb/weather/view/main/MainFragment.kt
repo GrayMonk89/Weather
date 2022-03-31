ab6897656804c3bee95e97eb58104e65de416c6e
graymonk89 <graymonk89@yandex.ru>
2022-03-31 16:12:50 +0300

    Урок 2. Добавил observe

--- app/src/main/java/com/gb/weather/view/main/MainFragment.kt
+++ app/src/main/java/com/gb/weather/view/main/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gb.weather.R
 
@@ -17,6 +19,18 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = object:Observer<Any> {
+            override fun onChanged(data: Any) {
+                renderData(data)
+            }
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data:Any){
+        Toast.makeText(requireContext(),"Bang", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateView(
--- app/src/main/java/com/gb/weather/view/main/MainViewModel.kt
+++ app/src/main/java/com/gb/weather/view/main/MainViewModel.kt
@@ -4,9 +4,17 @@ package com.gb.weather.view.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
 class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
     fun getData() : LiveData<Any>{
         return liveData
+    }
+
+    fun getWeather(){
+        Thread{
+            sleep(1500L)
+            liveData.postValue(Any())
+        }.start()
     }
 }
