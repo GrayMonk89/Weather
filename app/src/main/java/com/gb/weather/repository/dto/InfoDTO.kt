360c9aed6c0c3f7e2937937b3fd9ff6e811d3ee6
graymonk89 <graymonk89@yandex.ru>
2022-04-18 15:17:22 +0300

    Урок 5. Создал DTO классы в репозитории

---
+++ app/src/main/java/com/gb/weather/repository/dto/InfoDTO.kt
@@ -0,0 +1,13 @@
+package com.gb.weather.repository.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class InfoDTO(
+    @SerializedName("lat")
+    val lat: Double,
+    @SerializedName("lon")
+    val lon: Double,
+    @SerializedName("url")
+    val url: String
+)
