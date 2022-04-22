e9e85319f8bb6e353c584fb4fc7613a5ceb9c82d
graymonk89 <graymonk89@yandex.ru>
2022-04-22 08:43:22 +0300

    Урок 6. Добавил вечный поток

--- app/src/main/java/com/gb/weather/lesson6/ThreadsFragment.kt
+++ app/src/main/java/com/gb/weather/lesson6/ThreadsFragment.kt
@@ -1,9 +1,13 @@
 package com.gb.weather.lesson6
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import com.gb.weather.databinding.FragmentThreadsBinding
 import java.lang.Thread.sleep
@@ -31,17 +35,55 @@ class ThreadsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.calculationButton.setOnClickListener() {
-            with(binding)
-            {
+        val myEternalThread = EternalThread()
+        var counter = 0
+        myEternalThread.start()
+        with(binding) {
+            val timeRate = editTextRate.text.toString().toLong()
+            calculationButton.setOnClickListener {
                 Thread {
-                    val timeRate = editTextRate.text.toString().toLong()
                     sleep(timeRate * 1000)
                     requireActivity().runOnUiThread() {
-                        textViewResult.text = "hard work $timeRate seconds"
+                        textViewResult.text = "hard work $timeRate seconds 1"
+                        createTextView("${Thread.currentThread().name} ${++counter}")
+                    }
+                    Handler(Looper.getMainLooper()).post() {
+                        textViewResult.text = "hard work $timeRate seconds 2"
+                        createTextView("${Thread.currentThread().name} ${++counter}")
                     }
                 }.start()
             }
+
+            buttonEternalThread.setOnClickListener {
+                myEternalThread.mHandler?.post() {
+                    requireActivity().runOnUiThread() {
+                        textViewEternalThread.text = "hard work $timeRate seconds 1"
+                        createTextView("${Thread.currentThread().name} ${++counter}")
+
+                    }
+                    Handler(Looper.getMainLooper()).post() {
+                        textViewEternalThread.text = "hard work $timeRate seconds 1"
+                        createTextView("${Thread.currentThread().name} ${++counter}")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun createTextView(name: String) {
+        binding.fragmentContainer.addView(TextView(requireContext()).apply {
+            text = name
+            textSize = 14f
+        })
+    }
+
+    class EternalThread : Thread() {
+        var mHandler: Handler? = null
+        override fun run() {
+            Looper.prepare()
+            mHandler = Handler(Looper.myLooper()!!)
+            Looper.loop()
         }
     }
 
