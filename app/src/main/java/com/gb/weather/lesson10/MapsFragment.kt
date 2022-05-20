d95c4e6d26a20e8aefbfe02f534ebbdb7241d685
graymonk89 <graymonk89@yandex.ru>
2022-05-21 01:34:18 +0300

    Урок 10. Проверка комита

--- app/src/main/java/com/gb/weather/lesson10/MapsFragment.kt
+++ app/src/main/java/com/gb/weather/lesson10/MapsFragment.kt
@@ -38,6 +38,7 @@ class MapsFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+
     }
 
     private val callback = OnMapReadyCallback { googleMap ->
